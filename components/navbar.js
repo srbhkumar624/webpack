c8c3fbe9d5b095ca183544053ab7e51344f8347d
srbhkumar624 <srbhkumar624@gmail.com>
2022-04-28 19:10:21 +0530

    added pages

---
+++ components/navbar.js
@@ -0,0 +1,22 @@
+function navbar() {
+    return `
+    <h3>
+      <a href="/">Home</a>
+    </h3>
+    <div id="options">
+        <h3>
+            <a href="/jewelery.html">jewelery</a>
+        </h3>
+        <h3>
+        <a href="/electronics.html">Electronics</a>
+        </h3>
+        <h3>
+        <a href="loin.html">Login</a>
+        </h3>
+        <h3>
+        <a href="signup.html">Signup</a>
+        </h3>
+    </div>`
+
+}
+export { navbar }
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { navbar } from '../components/navbar';
+import '../styles/navbar.css';
+let navbar_div = document.getElementById("navbar")
+navbar_div.innerHTML = navbar();
+console.log(navbar_div);
